180998b9feb539aa5132fb4d049aa3010316c0db
Nobuyuki Nakano <nobuyuki.nakano.0123@gmail.com>
2020-07-21 20:37:16 +0900

    Created New

---
+++ prograbi/lion-9-web-api/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// -----------------------------------------------------------------------------------------------------
+// Ajaxリクエスト
+// -----------------------------------------------------------------------------------------------------
+function _onClickAjaxReqButton(e) {
+    _readUsersByAjaxAnd(1, createUsersList); // 1ページ目のデータを読み込みます
+}
+
+function _readUsersByAjaxAnd(pageNum, callback) {
+    pageNum = pageNum || 1; // pageNumが空であれば 1 を初期値にします
+    // 新しい XMLHttpRequest インスタンスを作成します
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://reqres.in/api/users?page=' + pageNum); // リクエスト方法, リクエスト先のURL
+
+    // リクエストの状態が変更したときに実行されるコールバック関数を設定します
+    xhr.onreadystatechange = function () {
+
+        // リクエストの状態で 4 以外は未完了のため、終了します
+        // https://developer.mozilla.org/ja/docs/Web/API/XMLHttpRequest/readyState
+        if (xhr.readyState !== 4) return;
+
+        if (xhr.status === 200) {
+            // リクエストのレスポンスコードが 200(OK) 
+            const jsObject = JSON.parse(xhr.responseText);  // レスポンス結果のJSONを JSオブジェクトに変換します
+            console.log(jsObject);                          // jsObjectを コンソールに出力します
+            callback(jsObject);
+        }
+        else {
+            // リクエストのレスポンスコードが 200以外(OKではない) 
+            console.log('HTTP error', xhr.status, xhr.statusText);
+        }
+    };
+
+    // リクエストを開始します
+    xhr.send();
+}
+
+// -----------------------------------------------------------------------------------------------------
+// Fetxhリクエスト
+// -----------------------------------------------------------------------------------------------------
+function _onClickFetchReqButton(e) {
+    _readUsersByFetchAnd(1, createUsersList);
+}
+
+function _readUsersByFetchAnd(pageNum, callback) {
+    pageNum = pageNum || 1; // pageNumが空であれば 1 を初期値にします
+    fetch('https://reqres.in/api/users?page=' + pageNum, { method: 'GET' })   // リクエスト先のURL, リクエスト方法などのオプション
+        .then(response => response.json())        // レスポンス結果(JSON)を JSオブジェクト に変更します
+        .then(jsObject => callback(jsObject))          // jsObjectを コンソールに出力します
+        .catch(error => console.error('error:', error));  // エラーの場合は コンソールに出力します
+}
+
+// -----------------------------------------------------------------------------------------------------
+// Common
+// -----------------------------------------------------------------------------------------------------
+function createUsersList(jsObject) {
+    const usersList = document.getElementById('usersList');
+
+    for (let i = 0; i < jsObject.data.length; i++) {
+        const user = jsObject.data[i];
+        console.log(user);
+        const usernameElement = document.createElement('h2');
+        usernameElement.innerText = user['first_name'] + ' ' + user['last_name'];
+        usersList.appendChild(usernameElement);
+    }
+}
---
+++ prograbi/lion-9/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * ボタンのクリックイベント処理
+ * @param {*} event 
+ */
+function _onClickFetchReq(event) {
+    console.log(event);
+    // fetchリクエスト関数を呼び出す
+    fecthUserReq();
+}
+
+function viewUsers(jsObject) {
+    console.log(jsObject.data);
+
+    for(let i = 0; i < jsObject.data.length; i++) {
+        // userデータを変数に代入する
+        const user = jsObject.data[i];
+
+        // userのフルネームを変数に代入する
+        const fullname = user.first_name + ' ' + user.last_name;
+
+        // ■ 作りたいDOM
+        // <div class="user">
+        //    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg" alt="user画像">
+        //    <span>name</span>
+        // </div>
+
+        // <div class="user">を作る
+        const userElement = document.createElement('div');
+        userElement.className = 'user';
+        // <img src="..." alt="...">を作る
+        const imgElement = document.createElement('img');
+        imgElement.src = user.avatar;
+        imgElement.alt = fullname + 'のアバター';
+        // <span>name</span>を作る
+        const userNameElement = document.createElement('span');
+        userNameElement.innerText = fullname;
+
+        // <div class="user"> に <img src="..." alt="..."> を挿れる
+        userElement.appendChild(imgElement);
+        // <div class="user"> に <span>name</span>を を挿れる
+        userElement.appendChild(userNameElement);
+        
+        // index.html の <div id="userList"></div> に作ったDOMを挿れる
+        document.getElementById('userList').appendChild(userElement);
+    }
+}
+
+/**
+ * Fetchリクエストを実行する.
+ * レスポンスJSオブジェクトを 関数viewUsers に渡す.
+ */
+function fecthUserReq() {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=キングダム', { method: 'GET' })   // リクエスト先のURL, リクエスト方法などのオプション
+        .then(response => response.json())        // レスポンス結果(JSON)を JSオブジェクト に変更します
+        .then(jsObject => console.log(jsObject))          // jsObjectを コンソールに出力します
+        .catch(error => console.error('error:', error));  // エラーの場合は コンソールに出力します
+}
