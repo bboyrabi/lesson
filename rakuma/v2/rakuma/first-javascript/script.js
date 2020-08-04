30412e03265313b9e70f1a30dcfe8e3e3a56d900
Nobuyuki Nakano <nobuyuki.nakano.0123@gmail.com>
2020-08-04 17:45:12 +0900

    Add new

---
+++ rakuma/v2/rakuma/first-javascript/script.js
@@ -0,0 +1,30 @@
+'use strict';
+console.log('1234' + 1);
+console.log(1234);
+
+console.log(`こんにちは${'みなさま'}`);
+
+let animal = 'dog';
+console.log(animal);
+
+animal = 'cat';
+console.log(animal);
+
+const apple = 'りんご';
+console.log(apple);
+
+// apple = 'ばなな'; // Error
+
+
+let num = 1;
+// num = num + 1;
+// num++;
+console.log(num++);
+console.log(num);
+
+// num = num + 20;
+num -= 20;
+console.log(num);
+
+let num2 = '-23';
+console.log(-num2 + 1);
