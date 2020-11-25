32e149c21d86220b59017dd6a02474832969fd68
Nobuyuki Nakano <nobuyuki.nakano.0123@gmail.com>
2020-11-25 16:25:20 +0900

    Updated Web API Demo

--- prograbi/lion-9/script.js
+++ prograbi/lion-9/script.js
@@ -1,59 +1,92 @@
 'use strict';
+// ----------------------------------------------------------------------------------------------------
+// 【JS】Web API 課題３
+// ----------------------------------------------------------------------------------------------------
+// GET LIST USERS
+requestByAjax('https://reqres.in/api/users?page=2', callbackOfAjax);
+// GET SINGLE USER
+requestByAjax('https://reqres.in/api/users/2', callbackOfAjax);
+// GET SINGLE USER NOT FOUND
+requestByAjax('https://reqres.in/api/users/23', callbackOfAjax);
+// GET <RESOURCE>
+requestByAjax('https://reqres.in/api/unknown', callbackOfAjax);
+// GET SINGLE <RESOURCE>
+requestByAjax('https://reqres.in/api/unknown/2', callbackOfAjax);
+// GET SINGLE <RESOURCE> NOT FOUND
+requestByAjax('https://reqres.in/api/unknown/23', callbackOfAjax);
 
 /**
- * ボタンのクリックイベント処理
- * @param {*} event 
+ * Ajaxのコールバック関数です。
+ * {@link requestByAjax}関数に引数として関数を渡す場合、
+ * xhrにはthisでアクセスすることができます。
  */
-function _onClickFetchReq(event) {
-    console.log(event);
-    // fetchリクエスト関数を呼び出す
-    fecthUserReq();
-}
+function callbackOfAjax() {
+    // リクエストの状態で 4 以外は未完了のため、終了します。
+    // https://developer.mozilla.org/ja/docs/Web/API/XMLHttpRequest/readyState
+    if (this.readyState !== 4) return;
 
-function viewUsers(jsObject) {
-    console.log(jsObject.data);
-
-    for(let i = 0; i < jsObject.data.length; i++) {
-        // userデータを変数に代入する
-        const user = jsObject.data[i];
-
-        // userのフルネームを変数に代入する
-        const fullname = user.first_name + ' ' + user.last_name;
-
-        // ■ 作りたいDOM
-        // <div class="user">
-        //    <img src="https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg" alt="user画像">
-        //    <span>name</span>
-        // </div>
-
-        // <div class="user">を作る
-        const userElement = document.createElement('div');
-        userElement.className = 'user';
-        // <img src="..." alt="...">を作る
-        const imgElement = document.createElement('img');
-        imgElement.src = user.avatar;
-        imgElement.alt = fullname + 'のアバター';
-        // <span>name</span>を作る
-        const userNameElement = document.createElement('span');
-        userNameElement.innerText = fullname;
-
-        // <div class="user"> に <img src="..." alt="..."> を挿れる
-        userElement.appendChild(imgElement);
-        // <div class="user"> に <span>name</span>を を挿れる
-        userElement.appendChild(userNameElement);
-        
-        // index.html の <div id="userList"></div> に作ったDOMを挿れる
-        document.getElementById('userList').appendChild(userElement);
+    if (this.status === 200) {
+        // リクエストのレスポンスコードが 200(OK) 
+        const jsObject = JSON.parse(this.responseText); // レスポンス結果(JSON)を JSオブジェクトに変換します。
+        console.log('Ajaxで取得しました。', jsObject);     // jsObjectを コンソールに出力します。
+    }
+    else {
+        // リクエストのレスポンスコードが 200以外(OKではない) 
+        console.log('HTTP error', this.status, this.statusText);
     }
 }
 
 /**
- * Fetchリクエストを実行する.
- * レスポンスJSオブジェクトを 関数viewUsers に渡す.
+ * Ajaxでリクエストします。
+ * 
+ * @param {*} url リクエスト先のURLです。
+ * @param {*} callback コールバック関数です。
  */
-function fecthUserReq() {
-    fetch('https://reqres.in/api/users?page=2', { method: 'GET' })   // リクエスト先のURL, リクエスト方法などのオプション
-        .then(response => response.json())        // レスポンス結果(JSON)を JSオブジェクト に変更します
-        .then(jsObject => viewUsers(jsObject))          // jsObjectを コンソールに出力します
-        .catch(error => console.error('error:', error));  // エラーの場合は コンソールに出力します
+function requestByAjax(url, callback) {
+    if (!url) return;
+    callback = callback || function () { }
+
+    // 新しい XMLHttpRequest インスタンスを作成します。
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url); // リクエスト方法, リクエスト先のURL
+
+    // リクエストの状態が変更したときに実行されるコールバック関数を設定します。
+    xhr.onreadystatechange = callback
+
+    // リクエストを開始します(open していないと実行できません)
+    xhr.send();
+}
+
+// ----------------------------------------------------------------------------------------------------
+// 【JS】Web API 課題４
+// ----------------------------------------------------------------------------------------------------
+// GET LIST USERS
+requestByFetch('https://reqres.in/api/users?page=2')
+// GET SINGLE USER
+requestByFetch('https://reqres.in/api/users/2')
+// GET SINGLE USER NOT FOUND
+requestByFetch('https://reqres.in/api/users/23')
+// GET <RESOURCE>
+requestByFetch('https://reqres.in/api/unknown')
+// GET SINGLE <RESOURCE>
+requestByFetch('https://reqres.in/api/unknown/2')
+// GET SINGLE <RESOURCE> NOT FOUND
+requestByFetch('https://reqres.in/api/unknown/23')
+
+/**
+ * Fetchでリクエストします。
+ * @param {*} url リクエスト先のURLです。
+ */
+function requestByFetch(url) {
+    if (!url) return;
+    fetch(url)  // リクエスト先のURL, リクエスト方法などのオプション
+        .then(res => {
+            if (!res.ok)
+                // レスポンスが正常ではないのでエラーを発生させています。このエラーは下の.catchで処理されます。
+                throw new Error(`${url}: ${res.status}`);
+            // レスポンス結果(JSON)を JSオブジェクト に変更します。
+            return res.json();
+        })
+        .then(jsObj => console.log(`Fetchで取得しました。`, jsObj))    // jsObjectを コンソールに出力します。
+        .catch(err => console.error('error:', err));                // エラーの場合は コンソールに出力します。
 }
